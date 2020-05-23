379bd12ea42a92a63f84127cb4e5972c533eb58c
parksaew <parksaew@gmail.com>
2020-05-23 15:19:13 -0400

    added characters with dynamic height changes

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ let calcDiv = document.getElementById('calculation')
 let myHeightInput = document.getElementById('own-height')
 let otherHeightInput = document.getElementById('other-height')
 
-
-
 myHeightInput.addEventListener('change', hypotenuse) 
 otherHeightInput.addEventListener('change', hypotenuse) 
 
@@ -14,26 +12,43 @@ function hypotenuse(){
     var safeHypotenuse = 200
     var heightDiff = Math.abs(myHeight - otherHeight)
     var safeHorizontal = Math.round(Math.sqrt(Math.pow(safeHypotenuse, 2) - Math.pow(heightDiff, 2)),2)
-    calcDiv.innerHTML = `Step away ${safeHorizontal}cm away from the other person`
+    calcDiv.innerHTML = `Step away <span style="color:#fecea8">${safeHorizontal}cm</span> away from the other person`
 
     var isUserShorter = myHeight < otherHeight ? 1 : 0; //ternary operator: 1 if user is shorter 0 else
+
+    var appContainer = document.getElementById('app-area');
+    var canvasContainer = document.getElementById('visualization');
+    
+    canvasContainer.style.width = safeHorizontal + 200 + "px";
+    
+    if(isUserShorter == 1){
+        appContainer.style.height = otherHeight + 220 + "px";
+        canvasContainer.style.height = otherHeight + "px";
+    } else {
+        appContainer.style.height = myHeight + 220 + "px";
+        canvasContainer.style.height = myHeight + "px";
+    } 
+
+    
+         
+
     drawVisualization(safeHorizontal, heightDiff, isUserShorter, myHeight, otherHeight)
 }
 
 function drawVisualization(xWidth, yHeight, leftShorter, ownImageHeight, otherImageHeight){
+    var canvasDiv = document.getElementById('visualization-diagram');
+    canvasDiv.style.left = 70 + "px";
+    
     var canvas = document.getElementById('triangle'); 
-    canvas.width = xWidth + 100;
-    canvas.height = yHeight + 40;
+    canvas.width = xWidth + 80;
+    canvas.height = yHeight + 20;    
 
     var myImage = document.getElementById('user-image');
     myImage.height = ownImageHeight
-    //myImage.style.left = '400px'
 
-    var otherImage=document.getElementById('other-image');
-    otherImage.height = otherImageHeight;
+    var otherImage = document.getElementById('other-image');
+    otherImage.height = otherImageHeight; 
 
-    var canvasContainer = document.getElementById('visualization');
-    canvasContainer.width = xWidth + 120; 
 
     if (canvas.getContext){
         var context = canvas.getContext('2d');
@@ -46,7 +61,8 @@ function drawVisualization(xWidth, yHeight, leftShorter, ownImageHeight, otherIm
 
         if (leftShorter == 1){     
             
-            canvasContainer.height = otherImageHeight; //the div surrounding the persons and triangle //feels like this is not updating
+            //canvasContainer.height = otherImageHeight; //the div surrounding the persons and triangle //feels like this is not updating
+
 
             context.beginPath(); // Reset the current path            
             context.moveTo(startX, startY); // Starting point          
@@ -58,14 +74,14 @@ function drawVisualization(xWidth, yHeight, leftShorter, ownImageHeight, otherIm
             
             // Put the dimensions as text
             context.fillText(`${yHeight}cm`, startX + xWidth + 5, startY/2);
-            context.fillText(`${xWidth}cm`, (startX + xWidth)/2, startY + 15);
-            //context.rotate(-angle * Math.PI / 180); //I could calculate the angle but not important
             context.fillText(`200cm`, (startX + xWidth)/3.5, startY/2);
+            //context.font = "10px Roboto"            
+            context.fillText(`${xWidth}cm`, (startX + xWidth)/2, startY + 12);
 
 
         } else {
 
-            canvasContainer.height = ownImageHeight; //the div surrounding the persons and triangle
+            //canvasContainer.height = ownImageHeight; //the div surrounding the persons and triangle
 
             startX = 50
             context.beginPath(); 
@@ -74,9 +90,11 @@ function drawVisualization(xWidth, yHeight, leftShorter, ownImageHeight, otherIm
             context.lineTo(startX, startY)
             context.lineTo(startX + xWidth, startY);
             context.stroke();
-            context.fillText(`${yHeight}cm`, 5, startY/2);
-            context.fillText(`${xWidth}cm`, (startX + xWidth)/2, startY + 15);
-            context.fillText(`200cm`, (startX + xWidth) - (startX + xWidth)/3.5, startY/2);
+            context.fillText(`${yHeight}cm`, 5, startY/2);   
+            context.fillText(`200cm`, (startX + xWidth) - (startX + xWidth)/3.5, startY/2);         
+            //context.font = "10px Roboto"
+            context.fillText(`${xWidth}cm`, (startX + xWidth)/2, startY + 12);
+            
 
         }
 
